0825b29894a550108c570620ffcfbd133c24c2a2
Alicja <aobara96@gmail.com>
2018-05-27 12:57:27 +0200

    commit

---
+++ build/CMakeFiles/projekt1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/projekt1.dir/src/projekt1.cpp.o"
+  "projekt1.pdb"
+  "projekt1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/projekt1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- src/projekt.cpp
+++ src/projekt.cpp
@@ -1,81 +1,109 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
-#include <opencv2/highgui/highgui.hpp>
+#include <fstream>
 
 using namespace std;
 using namespace cv;
 
+// Directory containing positive sample images
+static string posSamplesDir = "/home/alicja/gnu/projekt_wma/data/asl_alphabet_train/A";
+// Set the file to write the features to
+static string featuresFile = "/home/alicja/gnu/projekt_wma/data/hog_A.txt";
 
-int show(Mat img)
-{
-    //Imshow
-    imshow("okno", img);
+//parametry do HOG
+static const Size trainingPadding = Size(0, 0);
+static const Size winStride = Size(0, 0);
+
+int show(Mat img, string okno){
+    imshow(okno, img);
     waitKey(0);
-
-    // Ret
+    cout<<"wielkość "<<okno<<" "<<img.size()<<endl;
     destroyAllWindows();
     return 0;
 }
 
-int main(int argc, char** argv)
+void zapis(string featuresFile, vector< float> featureVector){
+    fstream File;
+    File.open(featuresFile.c_str(), ios::out);
+    if (File.good() && File.is_open()) {
+        File << "# Use this file to train, e.g. SVMlight by issuing $ svm_learn -i 1 -a weights.txt " << featuresFile.c_str() << endl; // Remove this line for libsvm which does not support comments
+            // Calculate feature vector from current image file
+            if (!featureVector.empty()) {
+                for (unsigned int feature = 0; feature < featureVector.size(); ++feature) {
+                    File << " " << (feature + 1) << ":" << featureVector.at(feature);
+                }
+                File << endl;
+            }
+
+        printf("\n");
+        File.flush();
+        File.close();
+    } else {
+        printf("Error opening file '%s'!\n", featuresFile.c_str());
+
+    }
+
+}
+
+int main()
 {
-    string path = string(PROJECT_SOURCE_DIR) + "/data/asl_alphabet_test/A_test.jpg";
-    Mat input = imread(path); //,IMREAD_GRAYSCALE)
-    if (input.empty()) // Sprawdzenie, czy udalo sie otworzyc obraz z sciezka
-    {
-        cout << "ERROR: Can't open image" << endl;
-        return -1;
-    }
-    show(input);
 
-    //tło
+    //image load
+    Mat img1 = imread(string(PROJECT_SOURCE_DIR) + "/data/asl_alphabet_test/B_test.jpg");
+    show(img1,"img1");
+    Mat img2 = imread(string(PROJECT_SOURCE_DIR) + "/data/asl_alphabet_train/B/B2.jpg");
+    show(img2,"img2");
+
+    //rgb 2 gray
+    Mat img1_gray;
+    cvtColor(img1, img1_gray, CV_RGB2GRAY);
+
+    Mat img2_gray;
+    cvtColor(img2, img2_gray, CV_RGB2GRAY);
+
+    //resize smaller
+    Mat r_img1_gray;
+    resize(img1_gray, r_img1_gray, Size(64, 128));
+    Mat r_img2_gray;
+    resize(img2_gray, r_img2_gray, Size(64, 128));
+
+    show(r_img1_gray,"img1 gray r");
+    show(r_img2_gray,"img2 gray r");
+
+    //extractino hog feature
+    HOGDescriptor d1( Size(64,8), Size(8,8), Size(4,4), Size(4,4), 9);
+    HOGDescriptor d2( Size(64,8), Size(8,8), Size(4,4), Size(4,4), 9);
+    // Size(32,16), //winSize
+    // Size(8,8), //blocksize
+    // Size(4,4), //blockStride,
+    // Size(4,4), //cellSize,
+    // 9, //nbins,
+
+    //hog feature compute
+    vector< float> descriptorsValues1;
+    vector< Point> locations1;
+    d1.compute( r_img1_gray, descriptorsValues1, trainingPadding, winStride, locations1);
+    vector< float> descriptorsValues2;
+    vector< Point> locations2;
+    d2.compute( r_img2_gray, descriptorsValues2, trainingPadding, winStride, locations2);
+
+    //hog feature size
+    //cout << descriptorsValues1.size() << endl;
+
     /*
-    string pathbg = string(PROJECT_SOURCE_DIR) + "/data/asl_alphabet_test/nothing_test.jpg";
-    Mat bg = imread(pathbg); //,IMREAD_GRAYSCALE)
-    if (bg.empty()) // Sprawdzenie, czy udalo sie otworzyc obraz z sciezka
-    {
-        cout << "ERROR: Can't open image" << endl;
-        return -1;
-    }
-    show(bg);
+    //copy vector to mat
+    //create Mat
+    Mat A(descriptorsValues1.size(),1,CV_32FC1);
+    //copy vector to mat
+    memcpy(A.data,descriptorsValues1.data(),descriptorsValues1.size()*sizeof(float));
+    //create Mat
+    Mat B(descriptorsValues2.size(),1,CV_32FC1);
+    //copy vector to mat
+    memcpy(B.data,descriptorsValues2.data(),descriptorsValues2.size()*sizeof(float));
     */
-    Mat output = input.clone();
 
-    Mat img = input.clone();
-    //Mat img;
-    //absdiff(input, bg, img);
-    img.convertTo(img, CV_8UC1);
-    //img = Mat(input.rows, input.cols, CV_8UC1);
+    zapis(featuresFile, descriptorsValues1);
 
-    //Skala szarosci
-    cvtColor(img, img, CV_RGB2GRAY);
-    show(img);
-
-    //rozmycie
-    //blur(img, img, Size(3,3) );
-    //show(img);
-
-    // Dylataja z wykorzystaniem wlasnego elementu strukturalnego
-    int kernel_size=3;
-    float sigma = 0.3*((kernel_size-1)*0.5 - 1) + 0.8; // zobacz @getGaussianKernel w dokumentacji opencv
-    GaussianBlur(img, img, Size(kernel_size, kernel_size), sigma);
-    show(img);
-
-    // Progowanie adaptacyjne
-    adaptiveThreshold(img, img, 255, ADAPTIVE_THRESH_GAUSSIAN_C, THRESH_BINARY, 101,-35);
-    show(img);
-
-    // Binaryzacja
-    //threshold(img, img, 20, 255, THRESH_BINARY);
-
-    // Morfologiczne otwarcie
-    //morphologyEx(img, img, MORPH_OPEN, Mat());
-
-    // Wyznaczenie konturow
-    //vector<vector<Point> > contours;
-    //findContours(img, contours, CV_RETR_LIST, CV_CHAIN_APPROX_SIMPLE);
-
-    //show(img);
 
     return 0;
 }
